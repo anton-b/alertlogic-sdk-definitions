dd5363bd72be2f8fcb839b9708473dd115d68a60
Anton Benkevich <abenkevich@alertlogic.com>
2020-06-15 17:37:17 +0100

    Add stages

--- scripts/travis-automerge.sh
+++ scripts/travis-automerge.sh
@@ -28,11 +28,14 @@ git clone "https://github.com/$GITHUB_REPO" "$repo_temp"
 # shellcheck disable=SC2164
 cd "$repo_temp"
 
+printf 'Fetching branch %s to merge to %s\n' "$TRAVIS_PULL_REQUEST_BRANCH" "$BRANCH_TO_MERGE_INTO" >&2
+git fetch origin $TRAVIS_PULL_REQUEST_BRANCH:$TRAVIS_PULL_REQUEST_BRANCH
+
 printf 'Checking out %s\n' "$BRANCH_TO_MERGE_INTO" >&2
 git checkout "$BRANCH_TO_MERGE_INTO"
 
-printf 'Merging %s\n' "$TRAVIS_COMMIT" >&2
-git merge --ff-only "$TRAVIS_COMMIT"
+printf 'Merging %s\n' "$TRAVIS_PULL_REQUEST_BRANCH" >&2
+git merge --ff-only "$TRAVIS_PULL_REQUEST_BRANCH"
 
 printf 'Pushing to %s\n' "$GITHUB_REPO" >&2
 
