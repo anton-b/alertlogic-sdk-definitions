8874d9d2900a022449eb587dc40d9fab8c508745
Anton Benkevich <abenkevich@alertlogic.com>
2020-06-15 20:42:25 +0100

    Merge commit

--- scripts/travis-automerge.sh
+++ scripts/travis-automerge.sh
@@ -35,7 +35,7 @@ printf 'Checking out %s\n' "$BRANCH_TO_MERGE_INTO" >&2
 git checkout "$BRANCH_TO_MERGE_INTO"
 
 printf 'Merging %s\n' "$TRAVIS_PULL_REQUEST_BRANCH" >&2
-git merge --ff-only "$TRAVIS_PULL_REQUEST_BRANCH"
+git merge "$TRAVIS_PULL_REQUEST_BRANCH" --no-edit
 
 printf 'Pushing to %s\n' "$GITHUB_REPO" >&2
 
